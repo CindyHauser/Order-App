5bd57ef47b31042be855f6e59e34d79f9e4a7115
CindyHauser <cindy.hauser24@gmx.de>
2026-02-02 21:35:11 +0100

    added images to the object

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log(dishes);
 console.table(dishes);
+console.log(dishes.burger);
+
+function init() {
+    renderMenu();
+};
+
+function renderMenu() {
+    let menuSelectionRef = document.getElementById("menu_selection");
+    menuSelectionRef.innerHTML = "";
+
+    for (let indexMenu = 0; indexMenu < dishes.length; indexMenu++) {
+
+        menuSelectionRef.innerHtml += `${dishes[indexMenu]}`;
+    };
+}
--- scripts/db.js
+++ scripts/db.js
@@ -3,17 +3,20 @@ let dishes = {
         {
             "name": "Veggie Black Burger",
             "price": 16.90,
-            "description": "Gemixter Salat, Tomaten, Edamer, Pilze"
+            "description": "Gemixter Salat, Tomaten, Edamer, Pilze",
+            "image": "veggie-burger.png"
         },
         {
             "name": "Fleischberg",
             "price": 15.90,
-            "description": "Rind, Bacon, geräucherter Käse, Ketchup, BBQ Sauce"
+            "description": "Rind, Bacon, geräucherter Käse, Ketchup, BBQ Sauce",
+            "image": "meat-burger.png"
         },
         {
             "name": "Beef Red Burger",
             "price": 14.90,
-            "description": "Rind, Käse, Tomaten, Zwiebeln, Kopfsalat"
+            "description": "Rind, Käse, Tomaten, Zwiebeln, Kopfsalat",
+            "image": "red-burger.png"
         },
     ],
 
@@ -21,17 +24,20 @@ let dishes = {
         {
             "name": "Pizza Margherita",
             "price": 11.90,
-            "description": "Tomatensauce, Mozzarella"
+            "description": "Tomatensauce, Mozzarella",
+            "image": "pizza-margharita.png"
         },
         {
             "name": "Pizza Chorizo",
             "price": 13.90,
-            "description": "Tomatenscheiben, Mozzarella, Chorizo"
+            "description": "Tomatenscheiben, Mozzarella, Chorizo",
+            "image": "pizza-chorizo.png"
         },
         {
             "name": "Pizza Funghi",
             "price": 12.90,
-            "description": "Rote Zwiebeln, Oliven, Pilze, Mozzarella"
+            "description": "Rote Zwiebeln, Oliven, Pilze, Mozzarella",
+            "image": "pizza-funghi.png"
         },
     ],
 
@@ -39,17 +45,20 @@ let dishes = {
         {
             "name": "Warmer Rindfleisch-Rucola-Salat",
             "price": 16.90,
-            "description": "Rindfleisch, Rucola, Feldsalat, griechischer Feta, Kirschtomaten, sonnengetrocknete Tomaten, Balsamico-Dressing"
+            "description": "Rindfleisch, Rucola, Feldsalat, griechischer Feta, Kirschtomaten, sonnengetrocknete Tomaten, Balsamico-Dressing",
+            "image": "salad-warm.png"
         },
         {
             "name": "Kleiner grüner Salat",
             "price": 7.90,
-            "description": "Grüner Salat, Gurke, Karotten, Petersilie, Radieschen"
+            "description": "Grüner Salat, Gurke, Karotten, Petersilie, Radieschen",
+            "image": "salad-mini.png"
         },
         {
             "name": "Grüner Salat mit Meeresfrüchten",
             "price": 16.90,
-            "description": "Gemischter Salat, Kirschtomaten, rote Zwiebeln, Muscheln, Tintenfischringe, Garnelen, Dijon-Senf-Zitronen-Dressing mit Dill"
+            "description": "Gemischter Salat, Kirschtomaten, rote Zwiebeln, Muscheln, Tintenfischringe, Garnelen, Dijon-Senf-Zitronen-Dressing mit Dill",
+            "image": "salad-fish.png"
         },
     ]
 
